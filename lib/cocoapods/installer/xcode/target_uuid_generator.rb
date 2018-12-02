161325be030d47fbab6f1a0b9404dc1751a1e283
Sebastian Shanus <sebastians@squareup.com>
2018-12-01 17:45:41 -0800

    stabilize target uuids

---
+++ lib/cocoapods/installer/xcode/target_uuid_generator.rb
@@ -0,0 +1,21 @@
+module Pod
+  class Installer
+    class TargetUUIDGenerator < Xcodeproj::Project::UUIDGenerator
+      def generate_all_paths_by_objects(projects)
+        @paths_by_object = {}
+        all_objects = projects.flat_map(&:objects)
+        all_objects.each do |object|
+          @paths_by_object[object] = if object.is_a? Xcodeproj::Project::Object::AbstractTarget
+                                       Digest::MD5.hexdigest(object.name).upcase
+                                     else
+                                       object.uuid
+                                     end
+        end
+      end
+
+      def uuid_for_path(path)
+        path
+      end
+    end
+  end
+end
